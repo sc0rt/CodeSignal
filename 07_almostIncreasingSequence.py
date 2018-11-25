ce8ec10e2f1665dbc4716efc9b35d1fbde716ec3
Scott Garland <43211582+ScottGarland@users.noreply.github.com>
2018-11-24 19:06:00 -0500

    Create 07_almostIncreasingSequence.py

---
+++ 07_almostIncreasingSequence.py
@@ -0,0 +1,18 @@
+def almostIncreasingSequence(sequence):
+
+    def increasing(test):
+            for i in range(0, len(test) - 1):
+                if test[i] >= test[i+1]:
+                    return False
+            return True
+    
+    for i in range(0, len(sequence) - 1):
+        if sequence[i] >= sequence[i+1]:
+            test1 = sequence[:i] + sequence[i+1:]
+            test2 = sequence[:i+1] + sequence[i+2:]
+            if increasing(test1) == True:
+                return True
+            elif increasing(test2) == True:
+                return True
+            else:
+                return False
